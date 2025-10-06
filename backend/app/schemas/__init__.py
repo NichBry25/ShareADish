172d3a08161042f2dc6081a9f536a7dfdf162534
osten-antonio <ostenantonio1@gmail.com>
2025-10-07 00:12:08 +0700

    added nutrition endpoint

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .user import UserBase, UserCreate, UserDB, UserResponse
 from .recipe import RecipeBase, RecipeCreate, RecipeDB, RecipeResponse
 from .base import ErrorResponse
 from .recipe_ai import RecipeCreationBase, RecipeEditResponse, RecipeEditRequest, GenerateRequest
+from .nutrient import Nutrients, NutrientsRequest
